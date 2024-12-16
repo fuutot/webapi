3bd26a74b6a36ad86a0fabd4399b74a2356b96c6
fuutot <fuutt659@gmail.com>
2024-12-16 11:52:48 +0900

    update: 型ヒントを追加

--- sample/hint.py
+++ sample/hint.py
@@ -1,5 +1,5 @@
-def get_full_name(first_name, last_name):
-    full_name = first_name.title() + " " + last_name.title()
+def get_full_name(first_name: str, last_name: str):
+    full_name = first_name.title() + " " + last_name.title
     return full_name
 
 
