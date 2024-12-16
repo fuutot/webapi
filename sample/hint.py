f7c13eaae1884ddf2e182597249736a0938ffecf
fuutot <fuutt659@gmail.com>
2024-12-16 14:30:21 +0900

    add: クラスを型として扱う

--- sample/hint.py
+++ sample/hint.py
@@ -27,5 +27,15 @@ def say_hi(name: Optional[str] = None): # デフォルトはNone
     else:
         print("Hello World")
 
-say_hi()
-say_hi("John")
+
+class Person:
+    def __init__(self, name: str):
+        self.name = name
+
+
+def get_person_name(one_person: Person):
+    return one_person.name
+
+
+david = Person("David")
+print(get_person_name(david))
