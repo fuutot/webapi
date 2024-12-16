09cb363d7010b41c76dec5cdada90e8f40c2943f
fuutot <fuutt659@gmail.com>
2024-12-16 17:09:33 +0900

    update: taskとdoneを管理に追加

--- api/main.py
+++ api/main.py
@@ -1,26 +1,10 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
-from typing import Optional
+from api.routers import task, done
+
 
 app = FastAPI()
 
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Optional[bool] = None
-
-@app.get("/")
-def read_root(): # rootで待つ
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}") # パスパラメータの設定
-def read_item(item_id: int, q: Optional[str] = None): # qはクエリパラメータ. qである必要はない
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+# FastAPIインスタンスに読み込ませる。読み込ませることでドキュメントに現れる
+app.include_router(task.router)
+app.include_router(done.router)
