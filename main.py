14c8ae820371caa953ee882b6de0f4a5687fc5eb
fuutot <fuutt659@gmail.com>
2024-12-16 15:26:33 +0900

    update: pydanticを使ったvalidation

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 from typing import Optional
 
 app = FastAPI()
 
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
 
 @app.get("/")
 def read_root(): # rootで待つ
@@ -13,3 +19,8 @@ def read_root(): # rootで待つ
 @app.get("/items/{item_id}") # パスパラメータの設定
 def read_item(item_id: int, q: Optional[str] = None): # qはクエリパラメータ. qである必要はない
     return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
