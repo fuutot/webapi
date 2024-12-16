16e596d5dd945cd128b4bcb930494b5c539ee9a3
fuutot <fuutt659@gmail.com>
2024-12-16 17:27:35 +0900

    update: レスポンススキーマに従ってデータを返す

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,13 +1,16 @@
+from typing import List
+
 from fastapi import APIRouter
+
+import api.schemas.task as task_schema
 
 router = APIRouter() # APIRouterを使ってパスをまとめる
 # リソースごとにファイスを分けることでファイルが肥大化しても見通しが確保しやすくなる
 
 
-@router.get("/tasks")
+@router.get("/tasks", response_model=List[task_schema.Task]) # レスポンススキーマをセット。複数返すためリスト
 async def list_tasks():
-    pass
-
+    return [task_schema.Task(id=1, title="No1. task")] # ひとまずダミーデータを返す
 
 @router.post("/tasks")
 async def create_task():
