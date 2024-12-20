3b7013bbdc59d25b50fc4f685b7369b4ab8ac651
fuutot <fuutt659@gmail.com>
2024-12-20 16:52:18 +0900

    add: basic decorator

---
+++ sample/decorator.py
@@ -0,0 +1,13 @@
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        print('Do some bullshit BEFORE executing %s' % func.__name__)
+        func(*args, **kwargs)
+        print('Do some bullshit AFTER executing %s' % func.__name__)
+
+    return wrapper
+
+@decorator
+def a_function_requiring_decoration():
+    print('I am a bullshit function which needs decoration')
+
+a_function_requiring_decoration()
