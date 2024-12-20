ad4e8ba8326dd9ede89c3df04fb2fc1090e6f3f5
fuutot <fuutt659@gmail.com>
2024-12-20 17:07:01 +0900

    add: pythonではすべてがオブジェクト

--- sample/decorator.py
+++ sample/decorator.py
@@ -11,3 +11,28 @@ def a_function_requiring_decoration():
     print('I am a bullshit function which needs decoration')
 
 a_function_requiring_decoration()
+"""
+Do some bullshit BEFORE executing a_function_requiring_decoration
+I am a bullshit function which needs decoration
+Do some bullshit AFTER executing a_function_requiring_decoration
+"""
+
+# Pythonにおいてすべてがオブジェクトである
+
+# int型のオブジェクトである
+x = 10
+print(type(x))
+print(x.bit_length())
+
+# str型のオブジェクトである
+s = "hoge"
+print(type(s))
+print(s.capitalize())
+
+# function型のオブジェクトである
+
+def hoge():
+    """hoge関数"""
+    return "hoge"
+print(type(hoge))
+print(hoge.__doc__)
