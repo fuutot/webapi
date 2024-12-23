e68345f39178c0e65eeb7496790113cd76976ef9
fuutot <fuutt659@gmail.com>
2024-12-23 10:07:39 +0900

    update: 関数でデコレータを実装

--- api/schemas/task.py
+++ api/schemas/task.py
@@ -3,7 +3,16 @@ from typing import Optional
 from pydantic import BaseModel, Field # Fieldは付加情報を与える時に使う
 
 
-class Task(BaseModel): # FastAPIのスキーマモデルであることを表す
-    id: int
+class TaskBase(BaseModel):
     title: Optional[str] = Field(None, example="机の掃除をする") # 最初の引数はデフォルト値を表す。 exampleはフィールドの値の例
+
+
+class Task(TaskBase): # FastAPIのスキーマモデルであることを表す
+    id: int
     done: bool = Field(False, description="完了フラグ") # descriptionは説明
+
+    class Config:
+        orm_mode = True
+
+class TaskCreate(TaskBase):
+    pass
