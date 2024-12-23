71a6f53975063558fd105a43de96ac32f5fd73a5
fuutot <fuutt659@gmail.com>
2024-12-23 10:10:57 +0900

    update: 関数でデコレータを実装

--- sample/decorator.py
+++ sample/decorator.py
@@ -17,16 +17,24 @@ I am a bullshit function which needs decoration
 Do some bullshit AFTER executing a_function_requiring_decoration
 """
 
-def hi():
-    return "hi sei!"
- 
-def do_something_before_hi(func):
-    print("I am doing some bullshit before executing %s()" % func.__name__)
-    print(func())
-    
- 
-do_something_before_hi(hi)
-"""
-I am doing some bullshit before executing hi()
-hi sei!
-"""
+def a_new_decorator(func):
+    def wrapTheFunction():
+        print("I am doing some bullshit before executing %s()" % func.__name__)
+
+        func()
+
+        print("I am doing some bullshit after executing %s()" % func.__name__)
+
+    return wrapTheFunction
+
+def a_function_requiring_decoration():
+    print("I am the bullshit function which needs some decoration")
+
+print('デコレータをつける前：')
+a_function_requiring_decoration()
+
+# デコレータをつけている
+a_function_requiring_decoration = a_new_decorator(a_function_requiring_decoration)
+
+print('\nデコレータをつけた後：')
+a_function_requiring_decoration()
